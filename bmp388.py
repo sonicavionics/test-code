33c22c982812b64714512ae35317304fa303f6d3
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2025-01-13 15:16:36 -0500

    Add delay to BMP388 sensor reading loop

--- bmp388.py
+++ bmp388.py
@@ -9,3 +9,4 @@ bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c, address=0x76)
 while True:
     print("Pressure: {:6.1f}".format(bmp.pressure))
     print("Temperature: {:5.2f}".format(bmp.temperature))
+    time.sleep(1)
