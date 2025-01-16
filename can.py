d0ffdd247c913db042ddfaf9f5b84771766a880d
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2025-01-16 01:17:39 -0500

    update SPI pin configuration in can.py for MCP2515 support

--- can.py
+++ can.py
@@ -7,5 +7,5 @@ CS_PIN = 28
 cs = digitalio.DigitalInOut(board.GP17)
 cs.direction = digitalio.Direction.OUTPUT
 # SCK, MOSI, MISO
-spi = busio.SPI(board.GP20, board.GP19, board.GP18)
+spi = busio.SPI(board.GP22, board.GP19, board.GP20)
 mcp = adafruit_mcp2515.MCP2515(spi, cs)
