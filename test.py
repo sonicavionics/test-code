1a54a15fbb7d7faea799cd04f6f0a36e011d87ec
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2025-01-29 02:14:19 -0500

    Remove reset2.py and refactor test.py to implement CAN bus communication with MCP2515

--- test.py
+++ test.py
@@ -1,37 +1,52 @@
+from time import sleep
+
 import board
+
 import busio
-import digitalio
 
-# Create the SPI bus
-spi = busio.SPI(board.GP20, board.GP19, board.GP18)
+from digitalio import DigitalInOut
 
-# Create a chip-select pin
-cs = digitalio.DigitalInOut(board.GP17)
-cs.direction = digitalio.Direction.OUTPUT
-cs.value = True  # Deselect by default
+from adafruit_mcp2515.canio import Message, RemoteTransmissionRequest
 
-# Prepare a buffer for reads (for example, 4 bytes)
-read_buffer = bytearray(4)
+from adafruit_mcp2515 import MCP2515 as CAN
 
-# Lock the SPI and configure settings
-while not spi.try_lock():
-    pass
-try:
-    # Configure your SPI parameters: 
-    # baudrate, polarity=0 or 1, phase=0 or 1
-    spi.configure(baudrate=1_000_000, phase=0, polarity=0)
-    
-    # ----- Writing to SPI -----
-    cs.value = False  # Assert CS (low)
-    spi.write(b"\x9F")  # Example: send the 'Read ID' command
-    cs.value = True   # De-assert CS (high)
 
-    # ----- Reading from SPI -----
-    cs.value = False  # Assert CS
-    spi.readinto(read_buffer)  # read 4 bytes into read_buffer
-    cs.value = True   # De-assert CS
+cs = DigitalInOut(board.GP17)
+cs.switch_to_output()
+spi = busio.SPI(board.GP22, board.GP19, board.GP20)
 
-finally:
-    spi.unlock()
+can_bus = CAN(  
 
-print("Read buffer:", read_buffer)
+    spi, cs, loopback=True, silent=True
+
+)  # use loopback to test without another device
+
+while True:
+
+    with can_bus.listen(timeout=1.0) as listener:
+
+        message = Message(id=0x1234ABCD, data=b"hellobro", extended=True)
+
+        send_success = can_bus.send(message)
+
+        print("Send success:", send_success)
+
+        message_count = listener.in_waiting()
+
+        print(message_count, "messages available")
+
+        for _i in range(message_count):
+
+            msg = listener.receive()
+
+            print("Message from ", hex(msg.id))
+
+            if isinstance(msg, Message):
+
+                print("message data:", msg.data)
+
+            if isinstance(msg, RemoteTransmissionRequest):
+
+                print("RTR length:", msg.length)
+
+    sleep(1)
